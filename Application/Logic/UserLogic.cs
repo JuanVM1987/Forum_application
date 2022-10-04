88fed13e0316342a45c7a9ab26238d0e297a8ddd
Juan <315208@via.dk>
2022-10-04 13:26:17 +0200

    add UserLogic.cs

--- Application/Logic/UserLogic.cs
+++ Application/Logic/UserLogic.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Application.DAOInterface;
+
+namespace Application.Logic;
+
+public class UserLogic
+{
+    private IUserDao _userDao;
+
+    public UserLogic(IUserDao userDao)
+    {
+        _userDao = userDao;
+    }
+}
